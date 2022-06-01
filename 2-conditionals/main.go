cec64d866041b659aee6bc681696073c28fb8d7c
OsmarPerez <osmar.perez@ifood.com.br>
2022-06-01 12:24:03 -0300

    adding conditionals

---
+++ 2-conditionals/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var option int
+
+	fmt.Println("Enter option:")
+	fmt.Scanf("%d", &option)
+
+	//diferente de C ele espera um boolean no if, não considerando números
+	if option == 1 {
+		fmt.Println("Option 1")
+	} else if option == 2 {
+		fmt.Println("Option 2")
+	} else if option == 3 {
+		fmt.Println("Option 3")
+	}
+
+	//não precisa colocar o break no switch em go
+	switch option {
+	case 1:
+		fmt.Println("Option 1")
+	case 2:
+		fmt.Println("option 2")
+	case 3:
+		fmt.Println("Option 3")
+	default:
+		fmt.Println("Undefined")
+	}
+}
