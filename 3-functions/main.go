b8806d35465219730c8e9670049ebf91136154fd
OsmarPerez <osmar.perez@ifood.com.br>
2022-06-01 15:22:34 -0300

    create functions

---
+++ 3-functions/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	showIntroduction()
+	option := inputOption()
+	fmt.Println("Option ", option, ".")
+
+}
+
+func showIntroduction() {
+	var nome string
+	var versao float32 = 1.1
+
+	fmt.Println("Digite seu nome:")
+	fmt.Scanf("%s", &nome)
+	fmt.Println("Olá ", nome)
+	fmt.Println("Versão do sistema", versao)
+
+}
+
+func inputOption() int {
+	var option int
+	fmt.Println("Chose the option: ")
+	fmt.Scanf("%d", &option)
+
+	switch option {
+	case 1:
+		fmt.Println("Option 1")
+	case 2:
+		fmt.Println("option 2")
+	case 3:
+		fmt.Println("Option 3")
+	default:
+		fmt.Println("Undefined")
+		os.Exit(-1)
+	}
+
+	return option
+
+}
